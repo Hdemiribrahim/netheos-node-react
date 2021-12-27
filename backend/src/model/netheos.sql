12435faa4cfefe9e7d2e5a4e321384069e430c7f
ucakmak02 <u.s.cakmak02@gmail.com>
2021-12-27 12:55:42 +0100

    database name is changed and readme redesigned

--- backend/src/model/netheos.sql
+++ backend/src/model/netheos.sql
@@ -18,7 +18,10 @@
 --
 -- Table structure for table `faq`
 --
-
+CREATE DATABASE `netheos_challenge_ugur`;
+USE `netheos_challenge_ugur`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
 DROP TABLE IF EXISTS `faq`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
